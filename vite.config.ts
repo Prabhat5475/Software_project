8fb65c4276699775b3f515b8ace5da337e9cbe71
Prabhat5475 <prabhat10choudhary@gmail.com>
2025-11-16 21:02:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Software_project/",   // 👈 ADD THIS LINE (IMPORTANT)
+   // 👈 ADD THIS LINE (IMPORTANT)
   plugins: [react()],
+  base: "./",
 });
